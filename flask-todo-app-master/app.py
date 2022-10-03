be87ad8a33e04ac5da10be90c117edd895bb853b
Goncalves <victorgoncalves@college.harvard.edu>
2022-10-03 14:53:32 -0400

    flask

---
+++ flask-todo-app-master/app.py
@@ -0,0 +1,68 @@
+import os
+from re import A
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'todo.db')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+confettiUpdate = False
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+
+@app.route("/")
+def index():
+    todo_list = Todo.query.all()
+
+    global confettiUpdate
+
+    confettiTmp = False
+    if confettiUpdate:
+        confettiTmp = True
+        confettiUpdate = False
+
+    return render_template("index.html", todo_list=todo_list, confettiUpdate=confettiTmp)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/complete/<string:todo_id>")
+def complete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+
+    global confettiUpdate
+
+    if todo.complete:
+        confettiUpdate = True
+
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<string:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
